90423dfb1d695b78c3f0d9f5ef0aa7a810c82890
Michael Waide <mvwaide@gmail.com>
2020-07-17 14:53:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,22 @@
 class Anagram
-attr_accessor: word
- def initialize(word)
-  @word = word
- end
 
-   def match(array_of_words)
-  array_of_words.select.do |element|
-  (@word.split("").sort) == (element.split("").sort)
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+
+
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
     end
- end
+  end
+
+
 end
 
 listen = Anagram.new("listen")
+
 listen.match(%w(enlists google inlets banana))
